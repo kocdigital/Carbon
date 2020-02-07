fa42e02e819ad5286932a05a7fb446041dcbac3e
Barış Velioğlu <barisvelioglu61@gmail.com>
2020-02-07 15:22:53 +0300

    .

--- Carbon.WebApplication/CarbonController.cs
+++ Carbon.WebApplication/CarbonController.cs
@@ -48,7 +48,7 @@ namespace Carbon.WebApplication
         }
         
         [ApiExplorerSettings(IgnoreApi = true)]
-        protected OkObjectResult PagedOk<T>(ICollection<T> entity)
+        protected OkObjectResult PagedOk<T>(PagedList<T> entity)
         {
             if (typeof(IPagedList).IsAssignableFrom(typeof(T)))
             {
