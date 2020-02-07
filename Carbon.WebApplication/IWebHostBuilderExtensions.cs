58341f6d6caa8de0a4eeacdb9368a6746147b84d
Barış Velioğlu <barisvelioglu61@gmail.com>
2020-02-07 16:51:50 +0300

    services interfaces

--- Carbon.WebApplication/IWebHostBuilderExtensions.cs
+++ Carbon.WebApplication/IWebHostBuilderExtensions.cs
@@ -9,9 +9,9 @@ namespace Carbon.WebApplication
 {
     public static class IWebHostBuilderExtensions
     {
-        public static void UseCarbonFeatures<TStartup>(this IWebHostBuilder builder)
+        public static void UseCarbonFeatures<TStartup>(this IWebHostBuilder builder) where TStartup : class
         {
-            var assemblyName = typeof(TStartup).Assembly.FullName;
+            var assemblyName = typeof(TStartup).Assembly.GetName().Name;
             var currentEnviroment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
             var consulAddress = Environment.GetEnvironmentVariable("CONSUL_ADDRESS");
 
