1a57d80d9facfbbdc07b99dacd9e07ee51768c2f
Barış Velioğlu <barisvelioglu61@gmail.com>
2020-02-13 17:26:23 +0300

    mapster extension

--- Carbon.PagedList.Mapster/PagedListExtensions.cs
+++ Carbon.PagedList.Mapster/PagedListExtensions.cs
@@ -6,7 +6,7 @@ namespace Carbon.PagedList.Mapster
 {
     public static class PagedListExtensions
     {
-        public static IPagedList<K> Adapt<T, K>(this PagedList<T> pagedList)
+        public static IPagedList<K> Adapt<T, K>(this IPagedList<T> pagedList)
         {
             return new StaticPagedList<K>(pagedList.ToList().Adapt<List<K>>(), pagedList);
         }
