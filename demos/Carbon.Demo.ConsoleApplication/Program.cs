4aafe8f1bb474ec0f0f1cf6cb0d55279268780e7
Barış Velioğlu <barisvelioglu61@gmail.com>
2020-02-21 17:39:16 +0300

    carbon demo application extended

--- demos/Carbon.Demo.ConsoleApplication/Program.cs
+++ demos/Carbon.Demo.ConsoleApplication/Program.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using MassTransit;
 using System;
 using Microsoft.Extensions.DependencyInjection;
+using Carbon.MassTransit;
+using RabbitMQ.Client;
 
 namespace Carbon.Demo.ConsoleApplication
 {
@@ -18,7 +20,35 @@ namespace Carbon.Demo.ConsoleApplication
                     {
                         services.AddMassTransit(cfg =>
                         {
-                            cfg.AddConsumer<TestConsumer>();
+                            cfg.AddConsumer<FlightPurchasedConsumer>();
+
+                            cfg.AddRabbitMqBus(hostContext.Configuration, (provider, busFactoryConfig) =>
+                            {
+                                string flightOrdersTopic = "flight-orders";
+
+                                busFactoryConfig.ReceiveEndpoint(flightOrdersTopic, configurator =>
+                                {
+                                    configurator.ExchangeType = ExchangeType.Topic;
+                                    configurator.Consumer<FlightPurchasedConsumer>(provider);
+                                });
+                            });
+
+                            cfg.AddServiceBus(hostContext.Configuration, (provider, busFactoryConfig) =>
+                            {
+                                string flightOrdersTopic = "flight-orders";
+
+                                string subscriptionName = "flight-subscriber";
+
+                                //bu isimde bir topic oluşturuyor
+                                busFactoryConfig.Message<FlightOrder>(m => { m.SetEntityName(flightOrdersTopic); });
+
+                                // setup Azure topic consumer
+                                //flight-subscriber isimli bir subscribeını flight-orders topici için oluşturuyor
+                                busFactoryConfig.SubscriptionEndpoint<FlightOrder>(subscriptionName, configurator =>
+                                {
+                                    configurator.Consumer<FlightPurchasedConsumer>(provider);
+                                });
+                            });
                         });
 
                         services.AddSingleton<IHostedService, MassTransitConsoleHostedService>();
