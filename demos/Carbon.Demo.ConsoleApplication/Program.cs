5623456d7a8fac8163200bba2e97afde143c8953
Barış Velioğlu <barisvelioglu61@gmail.com>
2020-02-26 21:45:41 +0300

    x

--- demos/Carbon.Demo.ConsoleApplication/Program.cs
+++ demos/Carbon.Demo.ConsoleApplication/Program.cs
@@ -6,9 +6,74 @@ using System;
 using Microsoft.Extensions.DependencyInjection;
 using Carbon.MassTransit;
 using RabbitMQ.Client;
+using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace Carbon.Demo.ConsoleApplication
 {
+    public interface ITestService
+    {
+        void ShowAlerts();
+    }
+
+    public class TestService : ITestService
+    {
+        private readonly ILogger<TestService> _logger;
+        public TestService(ILogger<TestService> logger)
+        {
+            _logger = logger;
+        }
+        public void ShowAlerts()
+        {
+            _logger.LogTrace("Warning !");
+            _logger.LogDebug("Debug !");
+            _logger.LogInformation("Information !");
+            _logger.LogWarning("Warning !");
+            _logger.LogError("Error !");
+            _logger.LogCritical("Critical !");
+        }
+    }
+
+    public class MyHostedService : IHostedService, IDisposable
+    {
+        private Timer _timer;
+        private ITestService _testService;
+
+        public MyHostedService(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Timed Background Service is starting.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+            TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            Console.WriteLine("Timed Background Service is working.");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Timed Background Service is stopping.2");
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _testService.ShowAlerts();
+            _timer?.Dispose();
+        }
+    }
+
+
     class Program
     {
         public static async Task Main()
@@ -18,6 +83,7 @@ namespace Carbon.Demo.ConsoleApplication
             var host = new HostBuilder()
                     .UseCarbonConfigureServices<Program>((hostContext, services) =>
                     {
+                        services.AddTransient<ITestService, TestService>();
                         services.AddMassTransitBus(cfg =>
                         {
                             cfg.AddConsumer<FlightPurchasedConsumer>();
@@ -50,6 +116,8 @@ namespace Carbon.Demo.ConsoleApplication
                                 });
                             });
                         });
+
+                        services.AddHostedService<MyHostedService>();
                     });
 
             await host.RunConsoleAsync();
