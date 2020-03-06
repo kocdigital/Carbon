def0ac8f1da5e66e4d51a6bfd4d29b9d138ef56d
Barış Velioğlu <barisvelioglu61@gmail.com>
2020-03-06 16:17:25 +0300

    x

---
+++ Carbon.MassTransit.Abstractions/MassTransitMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Carbon.MassTransit.Abstractions
+{
+    public class MassTransitMessage<T>
+    {
+        public MassTransitMessage(T message)
+        {
+            Message = message;
+            MessageType = new List<string>() { $"urn:message:{typeof(T).Namespace.Replace(".", ":")}" };
+        }
+        public List<string> MessageType { get; }
+        public T Message { get; }
+    }
+}
