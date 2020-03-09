70cd68791a0fbedbf7078ab821af309f55c22020
Barış Velioğlu <barisvelioglu61@gmail.com>
2020-03-09 16:39:32 +0300

    connectormessage

--- Carbon.MassTransit.Abstractions/MassTransitMessage.cs
+++ Carbon.MassTransit.Abstractions/MassTransitMessage.cs
@@ -4,12 +4,16 @@ namespace Carbon.MassTransit.Abstractions
 {
     public class MassTransitMessage<TContent, TMessage>
     {
+        public MassTransitMessage()
+        {
+
+        }
         public MassTransitMessage(TContent message)
         {
             Message = message;
             MessageType = new List<string>() { $"urn:message:{typeof(TMessage).FullName.Replace(".", ":")}" };
         }
-        public List<string> MessageType { get; }
-        public TContent Message { get; }
+        public List<string> MessageType { get; set; }
+        public TContent Message { get; set; }
     }
 }
