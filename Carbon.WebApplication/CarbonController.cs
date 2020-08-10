371cb9c035b5ca1e67164a65c713856b600ae616
keremaytac <kerem.aytac@kocdigital.com>
2020-08-10 17:40:55 +0300

    Obsolete methods added

--- Carbon.WebApplication/CarbonController.cs
+++ Carbon.WebApplication/CarbonController.cs
@@ -1,5 +1,6 @@
 ﻿using Carbon.Common;
 using Carbon.PagedList;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -174,5 +175,82 @@ namespace Carbon.WebApplication
             return Guid.NewGuid().ToString();
         }
 
+        [Obsolete("This method is obsolete. Usage for only apis dependant to P360Controller")]
+        protected OkObjectResult PagedOk<T>(T TEntity, OrdinatedPageDto ordinatedPageDto, int totalCount)
+        {
+            if (ordinatedPageDto.PageIndex > 1)
+            {
+                Response.Headers.AddParameter(Request, "X-Paging-Previous-Link", ordinatedPageDto.Ordination, ordinatedPageDto.PageSize, ordinatedPageDto.PageIndex - 1);
+            }
+            if (ordinatedPageDto.PageIndex < totalCount)
+            {
+                Response.Headers.AddParameter(Request, "X-Paging-Next-Link", ordinatedPageDto.Ordination, ordinatedPageDto.PageSize, ordinatedPageDto.PageIndex + 1);
+            }
+            return Ok(TEntity);
+        }
     }
+
+    [Obsolete("This class is obsolete. Usage for only apis dependant to P360Controller")]
+    public class OrdinatedPageDto
+    {
+        public List<Ordination> Ordination { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+
+        public OrdinatedPageDto()
+        {
+            PageSize = 250;
+            PageIndex = 1;
+            //Ordination = new List<Ordination>() { new Common.Ordination() {IsAscending = false, Value = "Id" } };
+        }
+    }
+
+    [Obsolete("This class is obsolete. Usage for only apis dependant to P360Controller")]
+
+    public class Ordination
+    {
+        public string Value { get; set; }
+        public bool IsAscending { get; set; }
+    }
+   
+    [Obsolete("This extension is obsolete. Usage for only apis dependant to P360Controller")]
+    public static class P360ControllerExtensionMethod
+    {
+        public static void AddParameter(this IHeaderDictionary headers, HttpRequest request, string key, List<Ordination> ordination, int pageSize, int pageIndex)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("?");
+            if (ordination != null)
+            {
+                for (int i = 0; i < ordination.Count; i++)
+                {
+                    if (builder.Length > 1)
+                        builder.Append("&");
+
+                    var value = ordination[i].Value;
+                    var isAscending = ordination[i].IsAscending;
+
+                    builder.Append(nameof(ordination)).Append("[").Append(i).Append("]").Append(".")
+                           .Append(nameof(value)).Append("=").Append(value)
+                           .Append("&")
+                           .Append(nameof(ordination)).Append("[").Append(i).Append("]").Append(".")
+                           .Append(nameof(isAscending)).Append("=").Append(isAscending.ToString());
+                }
+            }
+
+            if (builder.Length > 1)
+                builder.Append("&");
+
+            var headerParameterLink = builder.Append(nameof(pageSize)).Append("=").Append(pageSize)
+                                      .Append("&")
+                                      .Append(nameof(pageIndex)).Append("=").Append(pageIndex)
+                                      .ToString();
+
+            headerParameterLink = $"{request.Scheme}://{request.Host}{request.Path}{headerParameterLink}";
+            headers.Add(key, headerParameterLink);
+        }
+
+    }
+
 }
