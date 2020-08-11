a62e84dc17b64c85ba3052f8eb1be4303be69ff5
Mustafa Gördesli <02484829@kocsistem.onmicrosoft.com>
2020-08-11 11:26:36 +0300

    exception code added.

--- Carbon.ExceptionHandling/CarbonException{T}.cs
+++ Carbon.ExceptionHandling/CarbonException{T}.cs
@@ -13,24 +13,27 @@ namespace Carbon.ExceptionHandling.Abstractions
 
         public CarbonException(T code) : base(Convert.ToInt32(code), code.ToString())
         {
-
+            Code = code;
         }
 
         public CarbonException(T code, object model) : base(Convert.ToInt32(code), code.ToString(), model)
         {
+            Code = code;
         }
 
         public CarbonException(T code, params object[] arguments) : base(Convert.ToInt32(code), code.ToString(), arguments)
         {
-
+            Code = code;
         }
 
         public CarbonException(Exception innerException, T code) : base(innerException, Convert.ToInt32(code), code.ToString())
         {
+            Code = code;
         }
 
         public CarbonException(Exception innerException, T code, params object[] args) : base(innerException, Convert.ToInt32(code), code.ToString(), args)
         {
+            Code = code;
         }
     }
 }
