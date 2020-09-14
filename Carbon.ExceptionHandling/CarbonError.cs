760ba832e34534cb1e686ae44ce686475e36ce3f
Mustafa Gördesli <02484829@kocsistem.onmicrosoft.com>
2020-09-14 12:28:10 +0300

    Carbon error and validation added.

---
+++ Carbon.ExceptionHandling/CarbonError.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carbon.ExceptionHandling.Abstractions
+{
+    public class CarbonError
+    {
+        public string ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ Carbon.ExceptionHandling/CarbonValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Carbon.ExceptionHandling.Abstractions
+{
+    public class CarbonValidator<T, V> where T : AbstractValidator<V> where V : class
+    {
+        public List<CarbonError> Validate(V validatableClass)
+        {
+            T validator = (T)Activator.CreateInstance(typeof(T));
+            var results = validator.Validate(validatableClass);
+            var errors = new List<CarbonError>();
+
+            if (results.Errors.Any())
+            {
+                foreach (var innerError in results.Errors)
+                {
+                    errors.Add(new CarbonError()
+                    {
+                        ErrorCode = innerError.ErrorCode,
+                        ErrorMessage = innerError.ErrorMessage
+                    });
+                }
+            }
+            return errors;
+        }
+
+    }
+}
