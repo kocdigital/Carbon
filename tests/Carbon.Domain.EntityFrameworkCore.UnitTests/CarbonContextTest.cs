6adaa28f1d4dbdff0d89cda486904f2ed5a14193
Mustafa Gördesli <02484829@kocsistem.onmicrosoft.com>
2020-09-23 16:12:07 +0300

    Carbon.Domain.EntityFrameworkCore DbContext unit tests

---
+++ tests/Carbon.Domain.EntityFrameworkCore.UnitTests/CarbonContextTest.cs
@@ -0,0 +1,72 @@
+﻿using Carbon.Test.Common.DataShares;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using Moq;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Carbon.Domain.EntityFrameworkCore.UnitTests
+{
+    public class DbContextFacts : CarbonContextTest<DbContext> { public DbContextFacts(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { } }
+
+    public abstract class CarbonContextTest<TContext> : DbContext where TContext : DbContext
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        protected CarbonContextTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        #region SaveChanges
+        [Theory]
+        [SaveChanges]
+        public async Task SaveChanges_Successfully_Integer(bool acceptAllChangesOnSuccess)
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+            using (var context = new TestContext(options))
+            {
+                context.TestClass.Add(new TestClass { Id = Guid.NewGuid(), Name = "Name 1" });
+                context.TestClass.Add(new TestClass { Id = Guid.NewGuid(), Name = "Name 2" });
+                var response = context.SaveChanges(acceptAllChangesOnSuccess);
+                Assert.IsType<int>(response);
+            }
+
+
+            _testOutputHelper.WriteLine("Test passed!");
+        }
+        #endregion
+
+        #region SaveChangesAsync
+        [Theory]
+        [SaveChanges]
+        public async Task SaveChangesAsync_Successfully_Integer(bool acceptAllChangesOnSuccess)
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+            using (var context = new TestContext(options))
+            {
+                context.TestClass.Add(new TestClass { Id = Guid.NewGuid(), Name = "Name 1" });
+                context.TestClass.Add(new TestClass { Id = Guid.NewGuid(), Name = "Name 2" });
+                var response = await context.SaveChangesAsync(acceptAllChangesOnSuccess);
+                Assert.IsType<int>(response);
+            }
+
+
+            _testOutputHelper.WriteLine("Test passed!");
+        }
+        #endregion
+    }
+}
---
+++ tests/Common/Carbon.Test.Common/DataShares/CarbonContextDataShare.cs
@@ -0,0 +1,34 @@
+﻿using Carbon.Common;
+using Carbon.Domain.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Carbon.Test.Common.DataShares
+{
+    public class SaveChanges : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { true };
+        }
+    }
+
+    public class TestClass
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TestContext : CarbonContext<TestContext>
+    {
+        public TestContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TestClass> TestClass { get; set; }
+    }
+    
+}
