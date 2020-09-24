6c808aed52aacc05d9efe3dfb1ad5fab133aa40d
bilgehanpalalioglu <bilgehanpalalioglu@gmail.com>
2020-09-24 11:21:05 +0300

    abstract class unit test sample created

---
+++ tests/Common/Carbon.Test.Common/DataShares/EFRepositoryDataShare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Carbon.Test.Common.DataShares
+{
+
+    public class FoundEntityGetByIdAsyncEFRepository : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { Guid.Parse("06956e56-1f8b-4fd9-b4b4-f083c23bb98f")};
+        }
+    }
+    public class NotFoundEntityGetByIdAsyncEFRepository : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { Guid.NewGuid() };
+        }
+    }
+}
---
+++ tests/Common/Carbon.Test.Common/Fixtures/EFRepositoryFixture.cs
@@ -0,0 +1,26 @@
+﻿using Carbon.Test.Common.DataShares;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Carbon.Test.Common.Fixtures
+{
+    public class EFRepositoryFixture
+    {
+        public static TestCarbonContext CreateContext()
+        {
+            var options = new DbContextOptionsBuilder<TestCarbonContext>()
+         .UseInMemoryDatabase(databaseName: "TestDatabase")
+         .Options;
+            var context = new TestCarbonContext(options);
+
+            return context;
+        }
+
+        public static void CreateData(TestCarbonContext context, Guid id)
+        {
+            context.CarbonContextTestClass.Add(new CarbonContextTestClass { Id = id, Name = "Name 1" });
+            context.CarbonContextTestClass.Add(new CarbonContextTestClass { Id = Guid.NewGuid(), Name = "Name 2" });
+            context.SaveChanges();
+        }
+    }
+}
