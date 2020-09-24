0fbd908a3e758038a5e3e3db994fcb46437a7966
bilgehanpalalioglu <bilgehanpalalioglu@gmail.com>
2020-09-24 17:04:21 +0300

    100% coverage for ApiResponse

--- tests/Carbon.Common.UnitTests/ApiResponseTest.cs
+++ tests/Carbon.Common.UnitTests/ApiResponseTest.cs
@@ -1,4 +1,5 @@
 using Carbon.Common.UnitTests.DataShares;
+using Carbon.Common.UnitTests.Fixtures;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,21 +8,24 @@ using Xunit.Abstractions;
 
 namespace Carbon.Common.UnitTests
 {
-    public class DecimalFacts : ApiResponseTest<decimal> { public DecimalFacts(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { } }
-    public class StringFacts : ApiResponseTest<string> { public StringFacts(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { } }
-    public class DateTimeFacts : ApiResponseTest<DateTime> { public DateTimeFacts(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { } }
-    public class TimSpanFacts : ApiResponseTest<TimeSpan> { public TimSpanFacts(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { } }
-    public class Int32Facts : ApiResponseTest<int> { public Int32Facts(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { } }
-    public class ObjectFacts : ApiResponseTest<object> { public ObjectFacts(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { } }
-    public class ListStringFacts : ApiResponseTest<List<string>> { public ListStringFacts(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { } }
-  
+    public class DecimalFacts : ApiResponseTest<decimal>, IClassFixture<ApiResponseTestFixture> { public DecimalFacts(ITestOutputHelper testOutputHelper, ApiResponseTestFixture apiResponseTestFixture) : base(testOutputHelper, apiResponseTestFixture, apiResponseTestFixture.DecimalSampleValue) { } }
+    public class StringFacts : ApiResponseTest<string>, IClassFixture<ApiResponseTestFixture> { public StringFacts(ITestOutputHelper testOutputHelper, ApiResponseTestFixture apiResponseTestFixture) : base(testOutputHelper, apiResponseTestFixture, apiResponseTestFixture.StringSampleValue) { } }
+    public class DateTimeFacts : ApiResponseTest<DateTime>, IClassFixture<ApiResponseTestFixture> { public DateTimeFacts(ITestOutputHelper testOutputHelper, ApiResponseTestFixture apiResponseTestFixture) : base(testOutputHelper, apiResponseTestFixture, apiResponseTestFixture.DateTimeSampleValue) { } }
+    public class TimSpanFacts : ApiResponseTest<TimeSpan>, IClassFixture<ApiResponseTestFixture> { public TimSpanFacts(ITestOutputHelper testOutputHelper, ApiResponseTestFixture apiResponseTestFixture) : base(testOutputHelper, apiResponseTestFixture, apiResponseTestFixture.TimeSpanSampleValue) { } }
+    public class Int32Facts : ApiResponseTest<int>, IClassFixture<ApiResponseTestFixture> { public Int32Facts(ITestOutputHelper testOutputHelper, ApiResponseTestFixture apiResponseTestFixture) : base(testOutputHelper, apiResponseTestFixture, apiResponseTestFixture.IntSpanSampleValue) { } }
+    public class ObjectFacts : ApiResponseTest<object>, IClassFixture<ApiResponseTestFixture> { public ObjectFacts(ITestOutputHelper testOutputHelper, ApiResponseTestFixture apiResponseTestFixture) : base(testOutputHelper, apiResponseTestFixture, apiResponseTestFixture.ObjectSpanSampleValue) { } }
+    public class ListStringFacts : ApiResponseTest<List<string>>, IClassFixture<ApiResponseTestFixture> { public ListStringFacts(ITestOutputHelper testOutputHelper, ApiResponseTestFixture apiResponseTestFixture) : base(testOutputHelper, apiResponseTestFixture, apiResponseTestFixture.ListStringSpanSampleValue) { } }
+
     public abstract class ApiResponseTest<T>
     {
         private readonly ITestOutputHelper _testOutputHelper;
-
-        protected ApiResponseTest(ITestOutputHelper testOutputHelper)
+        private readonly ApiResponseTestFixture _apiResponseTestFixture;
+        private T Data { get; set; }
+        protected ApiResponseTest(ITestOutputHelper testOutputHelper, ApiResponseTestFixture apiResponseTestFixture, T data)
         {
             _testOutputHelper = testOutputHelper;
+            _apiResponseTestFixture = apiResponseTestFixture;
+            Data = data;
         }
         #region AddMessage
         [Theory]
@@ -46,10 +50,10 @@ namespace Carbon.Common.UnitTests
 
         [Theory]
         [AddNullMessageToApiResponse]
-        public void AddNullMessageTo_Successfully_ApiResponse(string identifier, ApiStatusCode statusCode, string message, int errorCode, IList<string> messages)
+        public void AddNullMessageTo_Successfully_ApiResponse(string identifier, ApiStatusCode statusCode, string message, int errorCode)
         {
             // Arrange
-            ApiResponse<T> response = new ApiResponse<T>(identifier, statusCode, errorCode, messages);
+            ApiResponse<T> response = new ApiResponse<T>(identifier, statusCode, errorCode);
 
             // Act
             response.AddMessage(message);
@@ -59,8 +63,6 @@ namespace Carbon.Common.UnitTests
             Assert.Equal(identifier, response.Identifier);
             Assert.Equal(statusCode, response.StatusCode);
             Assert.Equal(errorCode, response.ErrorCode);
-            Assert.Equal(messages, response.Messages);
-  
 
             _testOutputHelper.WriteLine("Test passed!");
         }
@@ -105,6 +107,100 @@ namespace Carbon.Common.UnitTests
             _testOutputHelper.WriteLine("Test passed!");
         }
 
+        #endregion
+        #region Constractor
+        [Theory]
+        [SetStatusAndIdentifierApiResponse]
+        public void SetStatusAndIdentifier_Successfully_ApiResponse(string identifier, ApiStatusCode statusCode)
+        {
+            // Arrange
+            // Act
+            ApiResponse<T> response = new ApiResponse<T>(identifier, statusCode);
+
+            // Assert
+            Assert.Equal(identifier, response.Identifier);
+            Assert.Equal(statusCode, response.StatusCode);
+
+            _testOutputHelper.WriteLine("Test passed!");
+        }
+
+        [Theory]
+        [SetStatusIdentifierMessagesApiResponse]
+        public void SetStatusIdentifierMessages_Successfully_ApiResponse(string identifier, ApiStatusCode statusCode, IList<string> messages)
+        {
+            // Arrange
+            // Act
+            ApiResponse<T> response = new ApiResponse<T>(identifier, statusCode, messages);
+
+            // Assert
+            Assert.Equal(identifier, response.Identifier);
+            Assert.Equal(statusCode, response.StatusCode);
+            Assert.Equal(messages, response.Messages);
+
+            _testOutputHelper.WriteLine("Test passed!");
+        }
+        #endregion
+        #region SetData
+        [Theory]
+        [SetStatusIdentifierMessagesApiResponse]
+        public void SetData_Successfully_ApiResponse(string identifier, ApiStatusCode statusCode, IList<string> messages)
+        {
+            // Arrange
+            // Act
+            ApiResponse<T> response = new ApiResponse<T>(identifier, statusCode, messages);
+
+            response.SetData(Data);
+            // Assert
+            Assert.Equal(identifier, response.Identifier);
+            Assert.Equal(statusCode, response.StatusCode);
+            Assert.Equal(messages, response.Messages);
+            Assert.Equal(response.Data, Data);
+
+            _testOutputHelper.WriteLine("Test passed!");
+        }
+        #endregion
+        #region SetErrorCode
+        [Theory]
+        [SetStatusIdentifierMessagesApiResponse]
+        public void SetErrorCode_Successfully_ApiResponse(string identifier, ApiStatusCode statusCode, IList<string> messages)
+        {
+            // Arrange
+            // Act
+            ApiResponse<T> response = new ApiResponse<T>(identifier, statusCode, messages);
+
+            response.SetErrorCode(_apiResponseTestFixture.ErrorCodeSample);
+            // Assert
+            Assert.Equal(identifier, response.Identifier);
+            Assert.Equal(statusCode, response.StatusCode);
+            Assert.Equal(messages, response.Messages);
+            Assert.Equal(response.ErrorCode, _apiResponseTestFixture.ErrorCodeSample);
+
+            _testOutputHelper.WriteLine("Test passed!");
+        }
+        #endregion
+        #region IsSuccess
+        [Theory]
+        [IsSuccessTrueApiResponse]
+        public void IsSuccessTrue_Successfully_ApiResponse(string identifier, ApiStatusCode statusCode)
+        {
+            // Arrange
+            // Act
+            ApiResponse<T> response = new ApiResponse<T>(identifier, statusCode);
+            // Assert
+            Assert.True(response.IsSuccess);
+            _testOutputHelper.WriteLine("Test passed!");
+        }
+        [Theory]
+        [IsSuccessFalseApiResponse]
+        public void IsSuccessFalse_Successfully_ApiResponse(string identifier, ApiStatusCode statusCode, int errorCode)
+        {
+            // Arrange
+            // Act
+            ApiResponse<T> response = new ApiResponse<T>(identifier, statusCode, errorCode);
+            // Assert
+            Assert.False(response.IsSuccess);
+            _testOutputHelper.WriteLine("Test passed!");
+        }
         #endregion
     }
 }
--- tests/Carbon.Common.UnitTests/DataShares/ApiResponseDataShare.cs
+++ tests/Carbon.Common.UnitTests/DataShares/ApiResponseDataShare.cs
@@ -6,6 +6,50 @@ using Xunit.Sdk;
 
 namespace Carbon.Common.UnitTests.DataShares
 {
+    public class IsSuccessTrueApiResponse : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK };
+        }
+    }
+    public class IsSuccessFalseApiResponse : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, 1 };
+        }
+    }
+    public class SetStatusAndIdentifierApiResponse : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.BadRequest };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.Conflict };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.InternalServerError };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.NotFound };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.RequestTimeout };
+            yield return new object[] { string.Empty, ApiStatusCode.UnAuthorized };
+            yield return new object[] { null, ApiStatusCode.UnAuthorized };
+        }
+    }
+    public class SetStatusIdentifierMessagesApiResponse : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, new List<string>() { "First Message", "Second Message" } };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, new List<string>() { "First Message", "Second Message" } };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.BadRequest, new List<string>() { "First Message", "Second Message" } };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.Conflict, new List<string>() { "First Message", "Second Message" } };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.InternalServerError, new List<string>() { "First Message", "Second Message" } };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.NotFound, null };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.RequestTimeout, new List<string>() { "First Message", "Second Message" } };
+            yield return new object[] { string.Empty, ApiStatusCode.UnAuthorized, new List<string>() { "First Message", "Second Message" } };
+            yield return new object[] { null, ApiStatusCode.UnAuthorized, new List<string>() { "First Message", "Second Message" } };
+        }
+    }
     public class AddValidMessageToApiResponse : DataAttribute
     {
         public override IEnumerable<object[]> GetData(MethodInfo testMethod)
@@ -28,21 +72,21 @@ namespace Carbon.Common.UnitTests.DataShares
             yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, 1, null };
         }
     }
-   
+
     public class AddNullMessageToApiResponse : DataAttribute
     {
         public override IEnumerable<object[]> GetData(MethodInfo testMethod)
         {
-            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, null, 1, new List<string>() { "First Message", "Second Message" } };
-            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, null, 1, new List<string>() { "First Message", "Second Message" } };
-            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, null, 1, new List<string>() { "First Message", "Second Message" } };
-            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.BadRequest, null, 1, new List<string>() { "First Message", "Second Message" } };
-            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.Conflict, null, 1, new List<string>() { "First Message", "Second Message" } };
-            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.InternalServerError, null, 1, new List<string>() { "First Message", "Second Message" } };
-            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.NotFound, null, 1, new List<string>() { "First Message", "Second Message" } };
-            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.RequestTimeout, null, 1, new List<string>() { "First Message", "Second Message" } };
-            yield return new object[] { string.Empty, ApiStatusCode.UnAuthorized, null, 1, new List<string>() { "First Message", "Second Message" } };
-            yield return new object[] { null, ApiStatusCode.UnAuthorized, null, 1, new List<string>() { "First Message", "Second Message" } };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, null, 1 };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, null, 1 };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.OK, null, 1 };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.BadRequest, null, 1 };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.Conflict, null, 1 };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.InternalServerError, null, 1 };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.NotFound, null, 1 };
+            yield return new object[] { Guid.NewGuid().ToString(), ApiStatusCode.RequestTimeout, null, 1 };
+            yield return new object[] { string.Empty, ApiStatusCode.UnAuthorized, null, 1 };
+            yield return new object[] { null, ApiStatusCode.UnAuthorized, null, 1 };
         }
     }
     public class AddValidMessagesToApiResponse : DataAttribute
