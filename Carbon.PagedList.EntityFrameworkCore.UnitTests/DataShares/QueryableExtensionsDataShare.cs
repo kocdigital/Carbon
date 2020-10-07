d5b316a2decf404eebc519e77b2692f6f2f2ec40
Gökhan Yiğit <gokhan.yigit@ext.kocdigital.com>
2020-10-07 12:10:24 +0300

    carbon.pagedlist.entityFrameworkCore unit tests

---
+++ Carbon.PagedList.EntityFrameworkCore.UnitTests/DataShares/QueryableExtensionsDataShare.cs
@@ -0,0 +1,34 @@
+﻿using Carbon.Test.Common.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Carbon.PagedList.EntityFrameworkCore.UnitTests.DataShares
+{
+    public class ToPagedListQueryableExtensions : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            var context = EFRepositoryFixture.CreateContext();
+            EFRepositoryFixture.CreateData(context, Guid.NewGuid(), Guid.NewGuid());
+
+            //data, pageNumber, pageSize
+            yield return new object[] { context.CarbonContextTestClass, 1, 1 };
+            yield return new object[] { null, 10, 2 };
+        }
+    }
+
+    public class InValidToPagedListQueryableExtensions : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            var context = EFRepositoryFixture.CreateContext();
+            EFRepositoryFixture.CreateData(context, Guid.NewGuid(), Guid.NewGuid());
+
+            //data, pageNumber, pageSize
+            yield return new object[] { context.CarbonContextTestClass, -1, -1 };
+            yield return new object[] { null, -10, -2 };
+        }
+    }
+}
---
+++ Carbon.PagedList.EntityFrameworkCore.UnitTests/PagedListExtensionsTest.cs
@@ -0,0 +1,51 @@
+﻿using Carbon.PagedList.EntityFrameworkCore.UnitTests.DataShares;
+using Carbon.PagedList.EntityFrameworkCore.UnitTests.StaticWrappers.QueryableExtensionsWrapper;
+using Carbon.Test.Common.DataShares;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Carbon.PagedList.EntityFrameworkCore.UnitTests
+{
+    public class StringEntity : PagedListExtensionsTest<CarbonContextTestClass> { public StringEntity(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { } }
+    public abstract class PagedListExtensionsTest<TEntity>
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        protected PagedListExtensionsTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Theory]
+        [ToPagedListQueryableExtensions]
+        public void ToPagedListAsync_Successfully_PagedListExtensions(IQueryable<TEntity> entity, int pageNumber, int pageSize)
+        {
+            // Arrange
+            // Act
+            var wrapper = new QueryableExtensionsWrapper<TEntity>();
+            var response = wrapper.ToPagedListAsync(entity, pageNumber, pageSize);
+
+            // Assert
+            Assert.Null(response.Exception);
+            Assert.IsType<PagedList<TEntity>>(response.Result);
+
+            _testOutputHelper.WriteLine("Test passed!");
+        }
+
+        [Theory]
+        [InValidToPagedListQueryableExtensions]
+        public void ToPagedListAsync_Exception_PagedListExtensions(IQueryable<TEntity> entity, int pageNumber, int pageSize)
+        {
+            // Arrange
+            // Act
+            var wrapper = new QueryableExtensionsWrapper<TEntity>();
+            var response = wrapper.ToPagedListAsync(entity, pageNumber, pageSize);
+
+            // Assert
+            Assert.NotNull(response.Exception);
+
+            _testOutputHelper.WriteLine("Test passed!");
+        }
+    }
+}
