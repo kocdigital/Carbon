745bec464adc4c720c296ca20b6f35e55ebe6f2b
Gökhan Yiğit <gokhan.yigit@ext.kocdigital.com>
2020-11-12 15:10:30 +0300

    carbon unit test internal service error object result and service collection extensions added

--- tests/Carbon.WebApplication.UnitTests/DataShares/InternalServerErrorObjectResultDataShare.cs
+++ tests/Carbon.WebApplication.UnitTests/DataShares/InternalServerErrorObjectResultDataShare.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Xunit.Sdk;
 
 namespace Carbon.WebApplication.UnitTests.DataShares
 {
-    public class IServiceCollectionExtensionsDataShare : DataAttribute
+    public class InternalServerErrorObjectResultData : DataAttribute
     {
         public override IEnumerable<object[]> GetData(MethodInfo testMethod)
         {
-            yield return new object[] { Guid.NewGuid().ToString() };
+            yield return new object[] { "sample" };
+            yield return new object[] { null };
         }
     }
 }
--- tests/Carbon.WebApplication.UnitTests/IServiceCollectionExtensionsTest.cs
+++ tests/Carbon.WebApplication.UnitTests/IServiceCollectionExtensionsTest.cs
@@ -3,7 +3,7 @@ using Carbon.WebApplication.UnitTests.StaticWrappers.IServiceCollectionConfigura
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
-using System.Threading.Tasks;
+using System;
 using Xunit;
 
 namespace Carbon.WebApplication.UnitTests
@@ -12,30 +12,39 @@ namespace Carbon.WebApplication.UnitTests
     {
         private readonly ConfigFixture _configFixture;
         private readonly Mock<IConfiguration> _configurationMock;
-        private readonly Mock<IServiceCollection> _serviceCollectionMock;
         public IServiceCollectionExtensionsTest(ConfigFixture configFixture)
         {
             _configFixture = configFixture;
             _configurationMock = new Mock<IConfiguration>();
-            _serviceCollectionMock = new Mock<IServiceCollection>();
         }
 
         [Fact]
         public void AddBearerAuthentication_Successfully_ServiceCollection()
         {
             // Arrange
+            var wrapper = new ServiceCollectionExtensionsWrapper();
+            IServiceCollection serviceCollection = new ServiceCollection();
             var config = _configFixture.GetConfiguration("Configs/configBearerAuthentication.json");
             _configurationMock.Setup(c => c.GetSection("JwtSettings")).Returns(config.GetSection("JwtSettings"));
+
             // Act
+            wrapper.AddBearerAuthentication(serviceCollection, _configurationMock.Object);
 
-            //TODO:
-            _serviceCollectionMock.Setup(c => c.AddAuthentication()).Returns(new Microsoft.AspNetCore.Authentication.AuthenticationBuilder(_serviceCollectionMock.Object));
-            ServiceCollectionExtensions.AddBearerAuthentication(_serviceCollectionMock.Object, _configurationMock.Object);
-
-            var httpStatus = new ServiceCollectionExtensionsWrapper();
-            var exception = Record.Exception(() => httpStatus.AddBearerAuthentication(_serviceCollectionMock.Object, _configurationMock.Object));
             // Assert      
-            Assert.Null(exception);            
+            Assert.IsType<ServiceCollectionExtensionsWrapper>(wrapper);
+        }
+
+        [Fact]
+        public void AddBearerAuthenticationToJwtSettingsNull_ThrowError_ServiceCollection()
+        {
+            // Arrange
+            var wrapper = new ServiceCollectionExtensionsWrapper();
+            IServiceCollection serviceCollection = new ServiceCollection();
+            var config = _configFixture.GetConfiguration("Configs/invalidConfigBearerAuthentication.json");
+            _configurationMock.Setup(c => c.GetSection("JwtSettings")).Returns(config.GetSection("JwtSettings"));
+
+            // Assert      
+            Assert.Throws<ArgumentNullException>(() => wrapper.AddBearerAuthentication(serviceCollection, _configurationMock.Object));
         }
     }
 }
