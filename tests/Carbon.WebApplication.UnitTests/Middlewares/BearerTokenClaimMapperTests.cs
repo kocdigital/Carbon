e1a6ce3e2feda3092898999b54e1af6f486c7335
Onur Varkıvanç <onur.varkivanc@kocdigital.com>
2020-11-19 14:42:22 +0300

    Added unit tests for Carbon.Webapplication middlewares

---
+++ tests/Carbon.WebApplication.UnitTests/Middlewares/BearerTokenClaimMapperTests.cs
@@ -0,0 +1,42 @@
+﻿using Carbon.WebApplication.Middlewares;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Carbon.WebApplication.UnitTests.Middlewares
+{
+    public class BearerTokenClaimMapperTests
+    {
+        public BearerTokenClaimMapperTests()
+        {
+
+        }
+
+        [Theory]
+        [MemberData(nameof(GetValuePairs))]
+        public void TryGetValue_MapSuccessfully(string key, string mappedKey, bool expectedResult)
+        {
+            // Arrange
+            string outString;
+
+            // Act
+            var res = BearerTokenClaimMapper.TryGetValue(key, out outString);
+
+            // Assert
+            Assert.Equal(outString, mappedKey);
+            Assert.Equal(res, expectedResult);
+        }
+
+        public static IEnumerable<object[]> GetValuePairs()
+        {
+            yield return new object[] { "sub", "ClientId", true };
+            yield return new object[] { "tenant-id", "TenantId", true };
+            yield return new object[] { "god-user", "GodUser", true };
+            yield return new object[] { "random non existant key", null, false };
+        }
+
+
+
+
+
+    }
+}
