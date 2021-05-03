05b78f69049994eec68268457d53c4edaf0149f9
bilgehanpalalioglu <bilgehanpalalioglu@gmail.com>
2021-05-03 12:14:00 +0300

    ScanKeysAndRemoveByPattern function added to redis extension. Keys method, shouldn't use in the production. Thats why implemented searching keys with scan.

--- Carbon.Redis/RedisConstants.cs
+++ Carbon.Redis/RedisConstants.cs
@@ -6,5 +6,9 @@
         public const string RedisKeyLengthKey = "redisKeyLength";
         public const string RedisSectionName = "Redis";
         public const string RedisDisabled = "Redis is disabled";
+        public const string Scan = "SCAN";
+        public const string Match = "MATCH";
+        public const string Count = "COUNT";
+        public const string SearchKeyCount = "500";
     }
 }
