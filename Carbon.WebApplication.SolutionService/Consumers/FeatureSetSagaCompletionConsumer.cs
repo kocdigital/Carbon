a696314995222cb7f813d34f53a79d95ea08336f
keremaytac <kerem.aytac@kocdigital.com>
2021-06-10 18:29:15 +0300

    improved

--- Carbon.WebApplication.SolutionService/Consumers/FeatureSetSagaCompletionConsumer.cs
+++ Carbon.WebApplication.SolutionService/Consumers/FeatureSetSagaCompletionConsumer.cs
@@ -1,4 +1,5 @@
-﻿using MassTransit;
+﻿using Carbon.WebApplication.SolutionService.Domain;
+using MassTransit;
 using Microsoft.Extensions.Logging;
 using Platform360.Domain.Messages;
 using Platform360.Domain.Messages.SolutionCreationSaga;
@@ -12,30 +13,46 @@ namespace Carbon.WebApplication.SolutionService.Consumers
     public class FeatureSetSagaCompletionSucceedConsumer : IConsumer<IFeatureSetCreationSucceed>
     {
         private ILogger<FeatureSetSagaCompletionSucceedConsumer> _logger;
-        public FeatureSetSagaCompletionSucceedConsumer(ILogger<FeatureSetSagaCompletionSucceedConsumer> logger)
+        private readonly RequestedObjects _requestedObjects;
+
+        public FeatureSetSagaCompletionSucceedConsumer(ILogger<FeatureSetSagaCompletionSucceedConsumer> logger, RequestedObjects requestedObjects)
         {
             _logger = logger;
+            _requestedObjects = requestedObjects;
+
         }
 
         public async Task Consume(ConsumeContext<IFeatureSetCreationSucceed> context)
         {
+
             var message = context.Message;
-            _logger.LogInformation($"FeatureSet has successfully registered to SolutionId: {message.SolutionId}, FeatureSet: {message.FeatureSetId}");
+
+            if (_requestedObjects.FeatureSets.Contains(message.CorrelationId))
+            {
+                _logger.LogInformation($"FeatureSet has successfully registered to SolutionId: {message.SolutionId}, FeatureSet: {message.FeatureSetId}");
+            }
         }
     }
 
     public class FeatureSetSagaCompletionFailedConsumer : IConsumer<IFeatureSetCreationFailed>
     {
         private ILogger<FeatureSetSagaCompletionFailedConsumer> _logger;
-        public FeatureSetSagaCompletionFailedConsumer(ILogger<FeatureSetSagaCompletionFailedConsumer> logger)
+        private readonly RequestedObjects _requestedObjects;
+
+        public FeatureSetSagaCompletionFailedConsumer(ILogger<FeatureSetSagaCompletionFailedConsumer> logger, RequestedObjects requestedObjects)
         {
             _logger = logger;
+            _requestedObjects = requestedObjects;
+
         }
 
         public async Task Consume(ConsumeContext<IFeatureSetCreationFailed> context)
         {
             var message = context.Message;
-            _logger.LogError($"FeatureSet registration has failed to SolutionId: {message.SolutionId}, FeatureSet: {message.FeatureSetId}, Error: {message.ErrorMessage}");
+            if (_requestedObjects.FeatureSets.Contains(message.CorrelationId))
+            {
+                _logger.LogError($"FeatureSet registration has failed to SolutionId: {message.SolutionId}, FeatureSet: {message.FeatureSetId}, Error: {message.ErrorMessage}");
+            }
         }
     }
 }
--- Carbon.WebApplication.SolutionService/Consumers/SolutionSagaCompletionConsumer.cs
+++ Carbon.WebApplication.SolutionService/Consumers/SolutionSagaCompletionConsumer.cs
@@ -1,4 +1,5 @@
-﻿using MassTransit;
+﻿using Carbon.WebApplication.SolutionService.Domain;
+using MassTransit;
 using Microsoft.Extensions.Logging;
 using Platform360.Domain.Messages;
 using Platform360.Domain.Messages.SolutionCreationSaga;
@@ -12,30 +13,43 @@ namespace Carbon.WebApplication.SolutionService.Consumers
     public class SolutionSagaCompletionSucceedConsumer : IConsumer<ISolutionCreationCompleted>
     {
         private ILogger<SolutionSagaCompletionSucceedConsumer> _logger;
-        public SolutionSagaCompletionSucceedConsumer(ILogger<SolutionSagaCompletionSucceedConsumer> logger)
+        private readonly RequestedObjects _requestedObjects;
+        public SolutionSagaCompletionSucceedConsumer(ILogger<SolutionSagaCompletionSucceedConsumer> logger, RequestedObjects requestedObjects)
         {
             _logger = logger;
+            _requestedObjects = requestedObjects;
         }
 
         public async Task Consume(ConsumeContext<ISolutionCreationCompleted> context)
         {
             var message = context.Message;
-            _logger.LogInformation($"Solution has successfully registered- SolutionId: {message.SolutionId}, SolutionName: {message.SolutionName}");
+
+            if (_requestedObjects.Solutions.Contains(message.CorrelationId))
+            {
+                _logger.LogInformation($"Solution has successfully registered- SolutionId: {message.SolutionId}, SolutionName: {message.SolutionName}");
+            }
         }
     }
 
     public class SolutionSagaCompletionFailedConsumer : IConsumer<ISolutionCreationFailed>
     {
         private ILogger<SolutionSagaCompletionFailedConsumer> _logger;
-        public SolutionSagaCompletionFailedConsumer(ILogger<SolutionSagaCompletionFailedConsumer> logger)
+        private readonly RequestedObjects _requestedObjects;
+
+        public SolutionSagaCompletionFailedConsumer(ILogger<SolutionSagaCompletionFailedConsumer> logger, RequestedObjects requestedObjects)
         {
             _logger = logger;
+            _requestedObjects = requestedObjects;
+
         }
 
         public async Task Consume(ConsumeContext<ISolutionCreationFailed> context)
         {
             var message = context.Message;
-            _logger.LogError($"Solution registration has failed- SolutionId: {message.SolutionId}, SolutionName: {message.SolutionName}, Error: {message.ErrorMessage}");
+            if (_requestedObjects.Solutions.Contains(message.CorrelationId))
+            {
+                _logger.LogError($"Solution registration has failed- SolutionId: {message.SolutionId}, SolutionName: {message.SolutionName}, Error: {message.ErrorMessage}");
+            }
         }
     }
 }
