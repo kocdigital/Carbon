a4a99d32b86427518c5462433702dbffe9ba1f5b
Anıl Eladağ <anil.eladag@kocdigital.com>
2021-06-28 17:32:12 +0300

    UsePasswordAuthenticator property added for connecting to CosmosDB

--- Carbon.Cassandra.Abstractions/ICassandraPersisterSettings.cs
+++ Carbon.Cassandra.Abstractions/ICassandraPersisterSettings.cs
@@ -3,36 +3,37 @@ using Cassandra.Mapping;
 
 namespace Carbon.Cassandra.Abstractions
 {
-    public interface ICassandraPersisterSettings
-    {
+	public interface ICassandraPersisterSettings
+	{
 
-        string[] EndPoints { get; set; }
+		string[] EndPoints { get; set; }
 
-        int Port { get; set; }
+		int Port { get; set; }
 
-        CompressionType? CompressionSupportType { get; set; }
+		CompressionType? CompressionSupportType { get; set; }
+		bool UsePasswordAuthenticator { get; set; }
 
-        string UserName { get; set; }
+		string UserName { get; set; }
 
-        string Password { get; set; }
+		string Password { get; set; }
 
-        string Keyspace { get; set; }
+		string Keyspace { get; set; }
 
-        Cluster Cluster { get; set; }
+		Cluster Cluster { get; set; }
 
-        IRetryPolicy RetryPolicy { get; set; }
+		IRetryPolicy RetryPolicy { get; set; }
 
-        IReconnectionPolicy ReconnectionPolicy { get; set; }
+		IReconnectionPolicy ReconnectionPolicy { get; set; }
 
-        PoolingOptions PoolingOptions { get; set; }
+		PoolingOptions PoolingOptions { get; set; }
 
-        QueryOptions QueryOptions { get; set; }
+		QueryOptions QueryOptions { get; set; }
 
-        ILoadBalancingPolicy LoadBalancingPolicy { get; set; }
+		ILoadBalancingPolicy LoadBalancingPolicy { get; set; }
 
-        void Build();
+		void Build();
 
-        void SetMapping(params Mappings[] mappings);
-    }
+		void SetMapping(params Mappings[] mappings);
+	}
 
 }
--- Carbon.Cassandra/CassandraPersisterSettings.cs
+++ Carbon.Cassandra/CassandraPersisterSettings.cs
@@ -1,121 +1,170 @@
 ﻿using Carbon.Cassandra.Abstractions;
+
 using Cassandra;
 using Cassandra.Mapping;
+
 using Microsoft.Extensions.Options;
-using Carbon.Cassandra.Abstractions;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Carbon.Cassandra
 {
 
-    public class CassandraPersisterSettings : ICassandraPersisterSettings, IOptions<CassandraPersisterSettings>
-    {
-        public CassandraPersisterSettings Value => this;
+	public class CassandraPersisterSettings : ICassandraPersisterSettings, IOptions<CassandraPersisterSettings>
+	{
+		public CassandraPersisterSettings Value => this;
 
-        public int Port { get; set; }
+		public int Port { get; set; }
 
-        public CompressionType? CompressionSupportType { get; set; }
+		public CompressionType? CompressionSupportType { get; set; }
 
-        public string UserName { get; set; }
+		public string UserName { get; set; }
 
-        public string Password { get; set; }
+		public string Password { get; set; }
+		public bool UsePasswordAuthenticator { get; set; }
 
-        public string[] EndPoints { get; set; }
-        public string Keyspace { get; set; }
+		public string[] EndPoints { get; set; }
+		public string Keyspace { get; set; }
 
-        Cluster ICassandraPersisterSettings.Cluster { get; set; }
+		Cluster ICassandraPersisterSettings.Cluster { get; set; }
 
-        IRetryPolicy ICassandraPersisterSettings.RetryPolicy { get; set; }
+		IRetryPolicy ICassandraPersisterSettings.RetryPolicy { get; set; }
 
-        IReconnectionPolicy ICassandraPersisterSettings.ReconnectionPolicy { get; set; }
+		IReconnectionPolicy ICassandraPersisterSettings.ReconnectionPolicy { get; set; }
 
-        PoolingOptions ICassandraPersisterSettings.PoolingOptions { get; set; }
+		PoolingOptions ICassandraPersisterSettings.PoolingOptions { get; set; }
 
-        QueryOptions ICassandraPersisterSettings.QueryOptions { get; set; }
+		QueryOptions ICassandraPersisterSettings.QueryOptions { get; set; }
 
-        ILoadBalancingPolicy ICassandraPersisterSettings.LoadBalancingPolicy { get; set; }
+		ILoadBalancingPolicy ICassandraPersisterSettings.LoadBalancingPolicy { get; set; }
 
-        public void SetMapping(params Mappings[] mappings)
-        {
-            MappingConfiguration.Global.Define(mappings);
-        }
+		public void SetMapping(params Mappings[] mappings)
+		{
+			MappingConfiguration.Global.Define(mappings);
+		}
 
-        public void Build()
-        {
-            var settings = this as ICassandraPersisterSettings;
+		public void Build()
+		{
+			var settings = this as ICassandraPersisterSettings;
 
-            var cluster = settings.Cluster;
+			var cluster = settings.Cluster;
 
-            if (cluster is null)
-            {
-                var builder = Cluster.Builder();
+			if (cluster is null)
+			{
+				var builder = Cluster.Builder();
 
-                builder.AddContactPoints(settings.EndPoints);
-                
-                builder.WithPort(settings.Port);
+				builder.AddContactPoints(settings.EndPoints);
 
-                if (!string.IsNullOrEmpty(settings.UserName))
-                {
-                    builder.WithAuthProvider(new PlainTextAuthProvider(settings.UserName, settings.Password));
-                }
+				builder.WithPort(settings.Port);
 
-                #region Default Settings
+				if (settings.UsePasswordAuthenticator && !string.IsNullOrEmpty(settings.UserName) && !string.IsNullOrEmpty(settings.Password))
+				{
+					builder.WithCredentials(settings.UserName, settings.Password);					
+					builder.WithSSL(getSSLOptions());
+				}
+				else if (!string.IsNullOrEmpty(settings.UserName))
+				{
+					builder.WithAuthProvider(new PlainTextAuthProvider(settings.UserName, settings.Password));
+				}
 
-                if(settings.CompressionSupportType is null)
-                {
-                    settings.SetCompression(CompressionType.Snappy);
-                }
+				#region Default Settings
 
-                if (settings.PoolingOptions is null)
-                {
-                    settings.SetPoolingOptions(new PoolingOptions()
-                                                    .SetCoreConnectionsPerHost(HostDistance.Local, 3)
-                                                    .SetMaxSimultaneousRequestsPerConnectionTreshold(HostDistance.Local, 1500)
-                                                    .SetMaxConnectionsPerHost(HostDistance.Local, 8)
-                                                    .SetCoreConnectionsPerHost(HostDistance.Remote, 3)
-                                                    .SetHeartBeatInterval(6000)
-                                                    .SetMaxSimultaneousRequestsPerConnectionTreshold(HostDistance.Remote, 1500)
-                                                    .SetMaxConnectionsPerHost(HostDistance.Remote, 8));
-                }
+				if (settings.CompressionSupportType is null)
+				{
+					settings.SetCompression(CompressionType.Snappy);
+				}
 
-                if (settings.QueryOptions is null)
-                {
-                    settings.SetQueryOptions(new QueryOptions().SetConsistencyLevel(ConsistencyLevel.One));
-                }
+				if (settings.PoolingOptions is null)
+				{
+					settings.SetPoolingOptions(new PoolingOptions()
+													.SetCoreConnectionsPerHost(HostDistance.Local, 3)
+													.SetMaxSimultaneousRequestsPerConnectionTreshold(HostDistance.Local, 1500)
+													.SetMaxConnectionsPerHost(HostDistance.Local, 8)
+													.SetCoreConnectionsPerHost(HostDistance.Remote, 3)
+													.SetHeartBeatInterval(6000)
+													.SetMaxSimultaneousRequestsPerConnectionTreshold(HostDistance.Remote, 1500)
+													.SetMaxConnectionsPerHost(HostDistance.Remote, 8));
+				}
 
-                if (settings.RetryPolicy is null)
-                {
-                    settings.SetRetryPolicy(DowngradingConsistencyRetryPolicy.Instance);
-                }
+				if (settings.QueryOptions is null)
+				{
+					settings.SetQueryOptions(new QueryOptions().SetConsistencyLevel(ConsistencyLevel.One));
+				}
 
-                if (settings.ReconnectionPolicy is null)
-                {
-                    settings.SetReconnectionPolicy(new ConstantReconnectionPolicy(1000));
-                }
+				if (settings.RetryPolicy is null)
+				{
+					settings.SetRetryPolicy(DowngradingConsistencyRetryPolicy.Instance);
+				}
 
-                if (settings.LoadBalancingPolicy is null)
-                {
-                    settings.SetLoadBalancingPolicy(new RoundRobinPolicy());
-                }
+				if (settings.ReconnectionPolicy is null)
+				{
+					settings.SetReconnectionPolicy(new ConstantReconnectionPolicy(1000));
+				}
 
-                #endregion
+				if (settings.LoadBalancingPolicy is null)
+				{
+					settings.SetLoadBalancingPolicy(new RoundRobinPolicy());
+				}
 
-                builder.WithCompression(settings.CompressionSupportType.Value);
+				#endregion
 
-                builder.WithReconnectionPolicy(settings.ReconnectionPolicy);
+				builder.WithCompression(settings.CompressionSupportType.Value);
 
-                builder.WithRetryPolicy(settings.RetryPolicy);
-                
-                builder.WithPoolingOptions(settings.PoolingOptions);
+				builder.WithReconnectionPolicy(settings.ReconnectionPolicy);
 
-                builder.WithQueryOptions(settings.QueryOptions);
+				builder.WithRetryPolicy(settings.RetryPolicy);
 
-                builder.WithLoadBalancingPolicy(settings.LoadBalancingPolicy);
+				builder.WithPoolingOptions(settings.PoolingOptions);
 
-                cluster = builder.Build();
-                
-                this.SetCluster<ICassandraPersisterSettings>(cluster);
-            }
-        }
+				builder.WithQueryOptions(settings.QueryOptions);
 
-    }
+				builder.WithLoadBalancingPolicy(settings.LoadBalancingPolicy);
+
+				cluster = builder.Build();
+
+				this.SetCluster<ICassandraPersisterSettings>(cluster);
+			}
+		}
+		private SSLOptions getSSLOptions()
+		{
+			var settings = this as ICassandraPersisterSettings;
+			var sslOptions = new SSLOptions(SslProtocols.Tls12, true, ValidateServerCertificate);
+			//Some DNS servers (eg. our Azure DNS) do not support reverse-lookup
+			//https://datastax-oss.atlassian.net/browse/CSHARP-652?page=com.atlassian.jira.plugin.system.issuetabpanels%3Aall-tabpanel
+			var hostMap = new List<(string ip, string hostname)>();
+			foreach (var host in settings.EndPoints)
+			{
+				hostMap.AddRange(Dns.GetHostAddresses(host).Select(ip => (ip.ToString(), host)));
+			}
+			sslOptions.SetHostNameResolver(ipAdd =>
+			{
+				var ip = ipAdd.ToString();
+				foreach (var host in hostMap.Where(_ => _.ip == ip))
+				{
+					return host.hostname;
+				}
+
+				return ip;
+			});
+			return sslOptions;
+		}
+
+		private static bool ValidateServerCertificate(
+			object sender,
+			X509Certificate certificate,
+			X509Chain chain,
+			SslPolicyErrors sslPolicyErrors)
+		{
+			if (sslPolicyErrors == SslPolicyErrors.None)
+				return true;
+
+			// Do not allow this client to communicate with unauthenticated servers.
+			return false;
+		}
+	}
 }
