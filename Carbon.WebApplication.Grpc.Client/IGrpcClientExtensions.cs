dddaaf5af101c732aedaa7306d33fa281383ecdf
keremaytac <kerem.aytac@kocdigital.com>
2021-07-07 18:21:57 +0300

    Carbon GRPC Client added

---
+++ Carbon.WebApplication.Grpc.Client/IGrpcClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Carbon.WebApplication.Grpc.Client
+{
+    public static class IGrpcClientExtensions
+    {
+        public static void AddCarbonGrpcClient<T>(this IServiceCollection services, string grpcServerAddress) where T : ClientBase
+        {
+            services.AddGrpcClient<T>(o =>
+            {
+                o.Address = new Uri(grpcServerAddress);
+            }).AddInterceptor(() => new CarbonGrpcInterceptor());
+        }
+    }
+}
