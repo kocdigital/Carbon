b7c85638567fe598976a14e588d9aa3fe237c35f
keremaytac <kerem.aytac@kocdigital.com>
2021-08-25 12:35:31 +0300

    - Goduser Implementation added for tenant management  - Solution header is must (p360-solution-id) for tenant management adopters  - Goduser bypasses any Ownership Filter  - Some performance improvements for tenant management

---
+++ Carbon.ExceptionHandling/CarbonExceptionMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carbon.ExceptionHandling.Abstractions
+{
+    public static class CarbonExceptionMessages
+    {
+        public static readonly string OperationForbidden = "This Operation is Forbidden!";
+        public static readonly string OnlyGodUserOperation = "Only godusers are allowed for this operation";
+        public static readonly string SolutionHeaderMustBeSet = "Solution Header [p360-solution-id] must be set!";
+
+    }
+}
