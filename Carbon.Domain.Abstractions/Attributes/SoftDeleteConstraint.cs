77bc7c2bdf1976cfed4f5fcfaef32dfa59bda3cc
Anıl Eladağ <anil.eladag@kocdigital.com>
2022-02-02 17:49:06 +0300

    summary added

--- Carbon.Domain.Abstractions/Attributes/SoftDeleteConstraint.cs
+++ Carbon.Domain.Abstractions/Attributes/SoftDeleteConstraint.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Carbon.Domain.Abstractions.Attributes
 {
+    /// <summary>
+    /// Checks related entities; if related entity is not deleted or soft deleted, throws <see cref="InvalidOperationException"/>
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
     public sealed class SoftDeleteConstraint : ValidationAttribute
     {
