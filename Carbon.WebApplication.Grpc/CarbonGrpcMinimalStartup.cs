7496bdca2cadbc51c900602c4d1a441d538c1bcf
keremaytac <kerem.aytac@kocdigital.com>
2022-08-10 12:39:40 +0300

    grpc dotnet 6

---
+++ Carbon.WebApplication.Grpc/CarbonGrpcMinimalStartup.cs
@@ -0,0 +1,76 @@
+﻿#if NET6_0
+using Carbon.Common;
+using Carbon.WebApplication.TenantManagementHandler.Extensions;
+using Carbon.WebApplication.TenantManagementHandler.Services;
+using FluentValidation.AspNetCore;
+using HealthChecks.UI.Client;
+using Mapster;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Serilog;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbon.WebApplication.Grpc
+{
+    public static class CarbonGrpcMinimalStartup
+    {
+        private static bool _useAuthentication;
+        private static bool _useAuthorization;
+        /// <summary>
+        /// Represents a set of key/value application configuration properties.
+        /// </summary>
+        public static IConfiguration Configuration { get; set; }
+
+
+        public static void AddCarbonGrpcServices(this WebApplicationBuilder builder, Func<IServiceCollection, IServiceCollection> serviceCollector, bool useAuthentication = true, bool useAuthorization = true, List<Type> interceptors = null)
+        {
+            _useAuthorization = useAuthorization;
+            _useAuthentication = useAuthentication;
+            var services = builder.Services;
+            Configuration = builder.Configuration;
+            Console.WriteLine($"Carbon is compiled with .Net 6.0 with GRPC and working on .Net {System.Environment.Version.ToString()}. (Minimal api)");
+
+            CommonStartup.AddServiceBaseLogic(services, Configuration, _useAuthorization, interceptors);
+
+            serviceCollector(services);
+            builder.WebHost.UseCarbonFeatures();
+        }
+
+        public static void AddCarbonGrpcApplication(this Microsoft.AspNetCore.Builder.WebApplication app, Func<Microsoft.AspNetCore.Builder.WebApplication, Microsoft.AspNetCore.Builder.WebApplication> applicationCollector, Func<IEndpointRouteBuilder, IEndpointRouteBuilder> endpointRouteCollector = null)
+        {
+            var env = app.Environment;
+            CommonStartup.AddAppBase(app, env);
+
+            app = applicationCollector(app);
+
+            CommonStartup.AddAppAuths(app, _useAuthentication, _useAuthorization);
+
+            app.UseEndpoints(endpoints =>
+            {
+                if (endpointRouteCollector != null)
+                    endpointRouteCollector(endpoints);
+
+                CommonStartup.AddAppEndpoints(endpoints);
+            });
+        }
+    }
+}
+#endif
--- Carbon.WebApplication.Grpc/CarbonGrpcStartup.cs
+++ Carbon.WebApplication.Grpc/CarbonGrpcStartup.cs
@@ -1,15 +1,17 @@
 ﻿using Carbon.Common;
 using Carbon.WebApplication.Grpc.Interceptors;
 using Carbon.WebApplication.TenantManagementHandler.Services;
-
+using HealthChecks.UI.Client;
 using Mapster;
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 
 using Serilog;
@@ -23,230 +25,131 @@ using System.Linq;
 
 namespace Carbon.WebApplication.Grpc
 {
-	public abstract class CarbonGrpcStartup<TStartup> where TStartup : class
-	{
-		private bool _useAuthentication;
-		private bool _useAuthorization;
-		private CorsPolicySettings _corsPolicySettings;
-		private string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-		private List<Type> _interceptors;
-		/// <summary>
-		/// Provides information about the web hosting environment an application is running in.
-		/// </summary>
-		public IWebHostEnvironment Environment { get; }
+    public abstract class CarbonGrpcStartup<TStartup> where TStartup : class
+    {
+        private bool _useAuthentication;
+        private bool _useAuthorization;
+        private List<Type> _interceptors;
+        /// <summary>
+        /// Provides information about the web hosting environment an application is running in.
+        /// </summary>
+        public IWebHostEnvironment Environment { get; }
 
-		/// <summary>
-		/// Represents a set of key/value application configuration properties.
-		/// </summary>
-		public IConfiguration Configuration { get; }
-		protected IList<FilterDescriptor> _filterDescriptors = new List<FilterDescriptor>();
+        /// <summary>
+        /// Represents a set of key/value application configuration properties.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+        protected IList<FilterDescriptor> _filterDescriptors = new List<FilterDescriptor>();
 
-		/// <summary>
-		/// Adds operation filter.
-		/// </summary>
-		/// <typeparam name="T">Specifies the type of filter.</typeparam>
-		/// <param name="args">Arguments of the filter</param>
-		public void AddOperationFilter<T>(params object[] args) where T : IOperationFilter
-		{
-			_filterDescriptors.Add(new FilterDescriptor()
-			{
-				Type = typeof(T),
-				Arguments = args
-			});
-		}
+        /// <summary>
+        /// Adds operation filter.
+        /// </summary>
+        /// <typeparam name="T">Specifies the type of filter.</typeparam>
+        /// <param name="args">Arguments of the filter</param>
+        public void AddOperationFilter<T>(params object[] args) where T : IOperationFilter
+        {
+            _filterDescriptors.Add(new FilterDescriptor()
+            {
+                Type = typeof(T),
+                Arguments = args
+            });
+        }
 
-		/// <summary>
-		/// Constructor that initializes configuration and environment variables.
-		/// </summary>
-		/// <param name="configuration">Represents a set of key/value application configuration properties.</param>
-		/// <param name="environment">Provides information about the web hosting environment an application is running in.</param>
-		protected CarbonGrpcStartup(IConfiguration configuration, IWebHostEnvironment environment, List<Type> interceptors = null)
-		{
-			Configuration = configuration;
-			Environment = environment;
-			_interceptors = interceptors ?? new List<Type>();
-		}
+        /// <summary>
+        /// Constructor that initializes configuration and environment variables.
+        /// </summary>
+        /// <param name="configuration">Represents a set of key/value application configuration properties.</param>
+        /// <param name="environment">Provides information about the web hosting environment an application is running in.</param>
+        protected CarbonGrpcStartup(IConfiguration configuration, IWebHostEnvironment environment, List<Type> interceptors = null)
+        {
+            Configuration = configuration;
+            Environment = environment;
+            _interceptors = interceptors ?? new List<Type>();
+        }
 
-		/// <summary>
-		/// Constructor that initializes configuration, environment, useAuthentication variables.
-		/// </summary>
-		/// <param name="configuration">Represents a set of key/value application configuration properties.</param>
-		/// <param name="environment">Provides information about the web hosting environment an application is running in.</param>
-		/// <param name="useAuthentication">Indicates that use authentication or not</param>
-		protected CarbonGrpcStartup(IConfiguration configuration, IWebHostEnvironment environment, bool useAuthentication, List<Type> interceptors = null)
-		{
-			Configuration = configuration;
-			Environment = environment;
-			_useAuthentication = useAuthentication;
-			_interceptors = interceptors ?? new List<Type>();
-		}
+        /// <summary>
+        /// Constructor that initializes configuration, environment, useAuthentication variables.
+        /// </summary>
+        /// <param name="configuration">Represents a set of key/value application configuration properties.</param>
+        /// <param name="environment">Provides information about the web hosting environment an application is running in.</param>
+        /// <param name="useAuthentication">Indicates that use authentication or not</param>
+        protected CarbonGrpcStartup(IConfiguration configuration, IWebHostEnvironment environment, bool useAuthentication, List<Type> interceptors = null)
+        {
+            Configuration = configuration;
+            Environment = environment;
+            _useAuthentication = useAuthentication;
+            _interceptors = interceptors ?? new List<Type>();
+        }
 
-		/// <summary>
-		/// Constructor that initializes configuration, environment, useAuthentication and useAuthorization variables.
-		/// </summary>
-		/// <param name="configuration">Represents a set of key/value application configuration properties.</param>
-		/// <param name="environment">Provides information about the web hosting environment an application is running in.</param>
-		/// <param name="useAuthentication">Indicates that use authentication or not</param>
-		/// <param name="useAuthorization">Indicates that use authorization or not</param>
-		protected CarbonGrpcStartup(IConfiguration configuration, IWebHostEnvironment environment, bool useAuthentication, bool useAuthorization, List<Type> interceptors = null)
-		{
-			Configuration = configuration;
-			Environment = environment;
-			_useAuthentication = useAuthentication;
-			_useAuthorization = useAuthorization;
-			_interceptors = interceptors ?? new List<Type>();
-		}
+        /// <summary>
+        /// Constructor that initializes configuration, environment, useAuthentication and useAuthorization variables.
+        /// </summary>
+        /// <param name="configuration">Represents a set of key/value application configuration properties.</param>
+        /// <param name="environment">Provides information about the web hosting environment an application is running in.</param>
+        /// <param name="useAuthentication">Indicates that use authentication or not</param>
+        /// <param name="useAuthorization">Indicates that use authorization or not</param>
+        protected CarbonGrpcStartup(IConfiguration configuration, IWebHostEnvironment environment, bool useAuthentication, bool useAuthorization, List<Type> interceptors = null)
+        {
+            Configuration = configuration;
+            Environment = environment;
+            _useAuthentication = useAuthentication;
+            _useAuthorization = useAuthorization;
+            _interceptors = interceptors ?? new List<Type>();
+        }
 
-		/// <summary>
-		/// Decides and Configures the services at startup. 
-		/// </summary>
-		/// <param name="services">Specifies the contract for a collection of service descriptors.</param>
-		public void ConfigureServices(IServiceCollection services)
-		{
-			Console.WriteLine("Carbon starting with .Net 5.0 with GRPC");
-			services.AddGrpc(options =>
-			{
-				options.Interceptors.Add<CarbonRequestDtoInterceptor>();
-				foreach (var interceptor in _interceptors)
-				{
-					options.Interceptors.Add(interceptor);
-				}
-			});
-			
-			services.AddHeaderPropagation();
+        /// <summary>
+        /// Decides and Configures the services at startup. 
+        /// </summary>
+        /// <param name="services">Specifies the contract for a collection of service descriptors.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+#if NET6_0
+            Console.WriteLine($"Carbon is compiled with .Net 6.0 with GRPC and working on .Net {System.Environment.Version.ToString()}. (not minimal api)");
+#elif NET5_0
+			Console.WriteLine($"Carbon is compiled with .Net 5.0 with GRPC and working on .Net {System.Environment.Version.ToString()}.");
+#elif NETCOREAPP3_1
+            Console.WriteLine($"Carbon is compiled with .NetCore 3.1 with GRPC and working on .Net {System.Environment.Version.ToString()}.");
+#endif
+            CommonStartup.AddServiceBaseLogic(services, Configuration, _useAuthorization, _interceptors);
+            CustomConfigureServices(services);
+        }
 
-			services.AddOptions();
-			services.Configure<SerilogSettings>(Configuration.GetSection("Serilog"));
-			services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
-			services.AddHttpClient();
+        /// <summary>
+        /// Configures the application builder according to given environment
+        /// </summary>
+        /// <param name="app">Defines a class that provides the mechanisms to configure an application's request pipeline.</param>
+        /// <param name="env">Provides information about the web hosting environment an application is running in.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            CommonStartup.AddAppBase(app, env);
 
-			services.AddSingleton(Configuration);
-			services.AddScoped<IExternalService, ExternalService>();
-			TypeAdapterConfig.GlobalSettings.Default.PreserveReference(true);
+            CustomConfigure(app, env);
 
-			#region Serilog Settings
+            CommonStartup.AddAppAuths(app, _useAuthentication, _useAuthorization);
 
-			var _serilogSettings = Configuration.GetSection("Serilog").Get<SerilogSettings>();
-
-			if (_serilogSettings == null)
-				throw new ArgumentNullException("Serilog settings cannot be empty!");
-
-			Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(Configuration).CreateLogger();
-
-			#endregion
-			#region Cors Policy Settings
-
-			_corsPolicySettings = Configuration.GetSection("CorsPolicy").Get<CorsPolicySettings>();
-
-			if (_corsPolicySettings != null)
-			{
-				services.AddCors(options =>
-				{
-					options.AddPolicy(MyAllowSpecificOrigins,
-						builder =>
-						{
-							if (_corsPolicySettings.AllowAnyOrigin)
-							{
-								builder = builder.AllowAnyOrigin();
-							}
-							else if (_corsPolicySettings.Origins != null && _corsPolicySettings.Origins.Count > 0)
-							{
-								builder = builder.WithOrigins(_corsPolicySettings.Origins.ToArray());
-							}
-
-							if (_corsPolicySettings.AllowAnyMethods)
-							{
-								builder = builder.AllowAnyMethod();
-							}
-
-							if (_corsPolicySettings.AllowAnyHeaders)
-							{
-								builder = builder.AllowAnyHeader();
-							}
-							builder.WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
-						});
-				});
-
-			}
+            app.UseEndpoints(endpoints =>
+            {
+                GrpcConfigureServices(endpoints);
+                CommonStartup.AddAppEndpoints(endpoints);
+            });
+        }
 
 
-			#endregion
-			if (_useAuthorization)
-			{
-				services.AddAuthorization();
-			}
+        /// <summary>
+        /// A Custom abstract method that configures the services. 
+        /// </summary>
+        /// <param name="services">Specifies the contract for a collection of service descriptors.</param>
+        public abstract void CustomConfigureServices(IServiceCollection services);
 
-			services.AddHealthChecks();
-
-
-			CustomConfigureServices(services);
-		}
-
-		/// <summary>
-		/// Configures the application builder according to given environment
-		/// </summary>
-		/// <param name="app">Defines a class that provides the mechanisms to configure an application's request pipeline.</param>
-		/// <param name="env">Provides information about the web hosting environment an application is running in.</param>
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			app.UseHeaderPropagation();
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-
-			app.UseSerilogRequestLogging();
-
-			app.UseStaticFiles();
-			app.UseRouting();
-
-			CustomConfigure(app, env);
-
-			if (_corsPolicySettings != null && (_corsPolicySettings.AllowAnyOrigin || (_corsPolicySettings.Origins != null && _corsPolicySettings.Origins.Count > 0)))
-			{
-				app.UseCors(MyAllowSpecificOrigins);
-			}
-
-			if (_useAuthentication)
-			{
-				app.UseAuthentication();
-			}
-			if (_useAuthorization)
-			{
-				app.UseAuthorization();
-			}
-
-			app.UseGrpcWeb();
+        /// <summary>
+        ///  A Custom abstract method that configures the application builder according to given environment
+        /// </summary>
+        /// <param name="app">Defines a class that provides the mechanisms to configure an application's request pipeline.</param>
+        /// <param name="env">Provides information about the web hosting environment an application is running in.</param>
+        public abstract void CustomConfigure(IApplicationBuilder app, IWebHostEnvironment env);
 
 
+        public abstract void GrpcConfigureServices(IEndpointRouteBuilder endpoints);
 
-			app.UseEndpoints(endpoints =>
-			{
-				GrpcConfigureServices(endpoints);
-				endpoints.MapHealthChecks("/health");
-				endpoints.MapGet("/", async context =>
-				{
-					await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-				});
-			});
-		}
-
-
-		/// <summary>
-		/// A Custom abstract method that configures the services. 
-		/// </summary>
-		/// <param name="services">Specifies the contract for a collection of service descriptors.</param>
-		public abstract void CustomConfigureServices(IServiceCollection services);
-
-		/// <summary>
-		///  A Custom abstract method that configures the application builder according to given environment
-		/// </summary>
-		/// <param name="app">Defines a class that provides the mechanisms to configure an application's request pipeline.</param>
-		/// <param name="env">Provides information about the web hosting environment an application is running in.</param>
-		public abstract void CustomConfigure(IApplicationBuilder app, IWebHostEnvironment env);
-
-
-		public abstract void GrpcConfigureServices(IEndpointRouteBuilder endpoints);
-
-	}
+    }
 }
