23c4313274029667f6d5942096ec90739bd33213
keremaytac <kerem.aytac@kocdigital.com>
2022-08-19 12:12:23 +0300

    More performant PagedList

--- Carbon.PagedList/PagedList.cs
+++ Carbon.PagedList/PagedList.cs
@@ -23,7 +23,44 @@ namespace Carbon.PagedList
         /// 	The subset of items contained only within this one page of the superset.
         /// </summary>
         protected List<T> Subset = new List<T>();
-        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PagedList{T}"/> class that divides the supplied superset into subsets the size of the supplied pageSize. The instance then only containes the objects contained in the subset specified by index.
+        /// </summary>
+        /// <param name="superset">The collection of objects to be divided into subsets. If the collection implements <see cref="IQueryable{T}"/>, it will be treated as such.</param>
+        /// <param name="pageNumber">The one-based index of the subset of objects to be contained by this instance.</param>
+        /// <param name="pageSize">The maximum size of any individual subset.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The specified index cannot be less than zero.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The specified page size cannot be less than one.</exception>
+        public PagedList(IEnumerable<T> subset, int pageNumber, int pageSize, int totalCount)
+        {
+            if (pageNumber < 1)
+                throw new ArgumentOutOfRangeException("pageNumber", pageNumber, "PageNumber cannot be below 1.");
+            if (pageSize < 1)
+                throw new ArgumentOutOfRangeException("pageSize", pageSize, "PageSize cannot be less than 1.");
+
+            // set source to blank list if superset is null to prevent exceptions
+            TotalItemCount = totalCount;
+            PageSize = pageSize;
+            PageNumber = pageNumber;
+            PageCount = TotalItemCount > 0
+                        ? (int)Math.Ceiling(TotalItemCount / (double)PageSize)
+                        : 0;
+            HasPreviousPage = PageNumber > 1;
+            HasNextPage = PageNumber < PageCount;
+            IsFirstPage = PageNumber == 1;
+            IsLastPage = PageNumber >= PageCount;
+            FirstItemOnPage = (PageNumber - 1) * PageSize + 1;
+            var numberOfLastItemOnPage = FirstItemOnPage + PageSize - 1;
+            LastItemOnPage = numberOfLastItemOnPage > TotalItemCount
+                            ? TotalItemCount
+                            : numberOfLastItemOnPage;
+
+            // add items to internal list
+            if (subset != null && TotalItemCount > 0)
+                Subset.AddRange(subset);
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="PagedList{T}"/> class that divides the supplied superset into subsets the size of the supplied pageSize. The instance then only containes the objects contained in the subset specified by index.
         /// </summary>
