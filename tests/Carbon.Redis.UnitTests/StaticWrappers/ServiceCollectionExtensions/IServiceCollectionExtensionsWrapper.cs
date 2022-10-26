dd9c416492b5fdf9a676c00a474e0dc60771d1f1
keremaytac <kerem.aytac@kocdigital.com>
2022-10-26 17:24:16 +0300

    unittestfix

--- tests/Carbon.Redis.UnitTests/StaticWrappers/ServiceCollectionExtensions/IServiceCollectionExtensionsWrapper.cs
+++ tests/Carbon.Redis.UnitTests/StaticWrappers/ServiceCollectionExtensions/IServiceCollectionExtensionsWrapper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Carbon.Redis.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,7 +9,7 @@ namespace Carbon.Redis.UnitTests.StaticWrappers.ServiceCollectionExtensions
 {
     public interface IServiceCollectionExtensionsWrapper
     {
-       public IServiceCollection AddRedisPersister(IServiceCollection services, IConfiguration configuration);
+       public ICarbonRedisBuilder AddRedisPersister(IServiceCollection services, IConfiguration configuration);
        public string ConvertToMD5(string password);
     }
 }
