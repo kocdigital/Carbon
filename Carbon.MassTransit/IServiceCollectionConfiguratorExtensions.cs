64e4571252dcb95e191fa487f6f29f7fc4456793
Kerem Aytaç <kerem.aytac@kocdigital.com>
2022-12-27 09:47:32 +0300

    Quick Fix (#86)
    quick fix

--- Carbon.MassTransit/IServiceCollectionConfiguratorExtensions.cs
+++ Carbon.MassTransit/IServiceCollectionConfiguratorExtensions.cs
@@ -314,10 +314,11 @@ namespace Carbon.MassTransit
                     foreach (var responseDestinationPath in responseDestinationPaths)
                     {
                         var consumerType = responseDestinationPath.Value;
+                        var dependencyConsumer = provider.GetRequiredService(responseDestinationPath.Value);
                         busFactoryConfig.ReceiveEndpoint("Req.Resp.Async-" + responseDestinationPath.Key, configurator =>
                         {
                             configurator.AddAsHighAvailableQueue(configuration);
-                            configurator.Consumer(consumerType, type => Activator.CreateInstance(consumerType));
+                            configurator.Consumer(consumerType, type => dependencyConsumer);
                         });
                     }
                 });
@@ -327,9 +328,10 @@ namespace Carbon.MassTransit
                     foreach (var responseDestinationPath in responseDestinationPaths)
                     {
                         var consumerType = responseDestinationPath.Value;
+                        var dependencyConsumer = provider.GetRequiredService(responseDestinationPath.Value);
                         busFactoryConfig.ReceiveEndpoint("Req.Resp.Async-" + responseDestinationPath, configurator =>
                         {
-                            configurator.Consumer(consumerType, type => Activator.CreateInstance(consumerType));
+                            configurator.Consumer(consumerType, type => dependencyConsumer);
                         });
                     }
                 });
