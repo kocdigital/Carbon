070babf5ff9251c7201f758b9bab4b135aad7a79
burak <buyukbaskinburak2@gmail.com>
2023-10-27 17:58:05 +0300

    added UnauthorizedOperation exception and its handling

--- Carbon.ExceptionHandling/CarbonExceptionMessages.cs
+++ Carbon.ExceptionHandling/CarbonExceptionMessages.cs
@@ -7,10 +7,10 @@ namespace Carbon.ExceptionHandling.Abstractions
     public static class CarbonExceptionMessages
     {
         public static readonly string OperationForbidden = "This Operation is Forbidden!";
+        public static readonly string OperationUnauthorized = "This Operation is Unauthorized!";
         public static readonly string OnlyGodUserOperation = "Only godusers are allowed for this operation";
         public static readonly string SolutionHeaderMustBeSet = "Solution Header [p360-solution-id] must be set!";
         public static readonly string OnlyAdminUserOperation = "Only admin users are allowed for this operation";
         public static readonly string OnlySuperAdminUserOperation = "Only superadmin users are allowed for this operation";
-
     }
 }
