12b0b1150baa1d5bea9111a4643c2e31f879db8f
02486363 <>
2023-12-28 14:55:31 +0300

    feat: Add multi get async support.

--- Carbon.Redis/Abstractions/ICarbonCacheExtensions.cs
+++ Carbon.Redis/Abstractions/ICarbonCacheExtensions.cs
@@ -4,6 +4,7 @@ using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading;
@@ -571,6 +572,32 @@ namespace Carbon.Caching.Abstractions
         }
 
         #region IPlatform360Cache - async
+        
+        /// <summary>
+        /// Simple awaitable get multi async operation.
+        /// </summary>
+        /// <typeparam name="T">Any object type that is serializable</typeparam>
+        /// <param name="instance"></param>
+        /// <param name="keys">Redis Keys as string list </param>
+        /// <param name="token">Cancellation Token</param>
+        /// <returns>Dictionary as key, value poir. Missing items won't be appear in the dictionary.</returns>
+        public static async Task<Dictionary<string,T>> GetMultiAsync<T>(this ICarbonCache instance, List<string> keys, CancellationToken token = default(CancellationToken)) where T : class
+        {
+            var redisKeys = keys.Select(k => new RedisKey(k)).ToArray();
+            var redisValues = await instance.GetDatabase().StringGetAsync(redisKeys);
+            var output = new Dictionary<string, T>();
+            int i = 0;
+            foreach (var redisValue in redisValues)
+            {
+                if (redisValue is { IsNull: false, HasValue: true })
+                {
+                    T result = Encoding.UTF8.GetBytes(redisValue.ToString()).FromByteArray<T>(CarbonContentSerializationType.Json);
+                    output.Add(redisKeys[i].ToString(),result);
+                }
+                i++;
+            }
+            return output;
+        }
 
         /// <summary>
         /// Simple awaitable get async operation which is inherited from IDistributedCache interface, all the get logic is managed by Microsoft.Extensions.Caching.Distributed.
