513bca145204e3275508277ffce54c327fce4fe4
anil eladag <68186458+anil-eladag@users.noreply.github.com>
2024-12-23 04:22:13 +0300

    Domain1 6 update (#152)
    * Bump version numbers and update dependent Carbon.Domain packages across multiple projects
    
    * Update preprocessor directive for .NET version compatibility in CarbonMinimalStartup.cs
    
    * Refactor: Remove redundant async methods from EFWithReadOnlyRepository classes
    
    * Refactor: Add CancellationToken parameter to asynchronous repository methods in Tests
    
    ---------
    
    Co-authored-by: anileladag <anil.eladag@kocdigital.com>

--- tests/Carbon.Domain.EntityFrameworkCore.UnitTests/EFRepositoryTest.cs
+++ tests/Carbon.Domain.EntityFrameworkCore.UnitTests/EFRepositoryTest.cs
@@ -28,7 +28,7 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
             // Arrange
             EFRepositoryFixture.CreateData(base.context, Id, Guid.NewGuid());
             // Act
-            var response = await base.GetByIdAsync(Id);
+            var response = await base.GetByIdAsync(Id, CancellationToken.None);
 
             // Assert
             Assert.Equal("Name 1", response.Name);
@@ -40,7 +40,7 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
             // Arrange
             EFRepositoryFixture.CreateData(base.context, Guid.NewGuid(), Guid.NewGuid());
             // Act
-            var response = await base.GetByIdAsync(Id);
+            var response = await base.GetByIdAsync(Id, CancellationToken.None);
 
             // Assert
             Assert.Null(response);
@@ -53,7 +53,7 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
             // Arrange
 
             // Act
-            var response = await base.CreateAsync(carbonContextTestClass);
+            var response = await base.CreateAsync(carbonContextTestClass, CancellationToken.None);
 
             // Assert
             Assert.Equal(carbonContextTestClass.Name, response.Name);
@@ -64,10 +64,10 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
         public async Task UpdateAsync_Successfully_ReturnEntity(CarbonContextTestClass carbonContextTestClass)
         {
             // Arrange
-            var createResponse = await base.CreateAsync(carbonContextTestClass);
+            var createResponse = await base.CreateAsync(carbonContextTestClass, CancellationToken.None);
             createResponse.Name = "Updated";
             // Act
-            var response = await base.UpdateAsync(createResponse);
+            var response = await base.UpdateAsync(createResponse, CancellationToken.None);
 
             // Assert
             Assert.Equal(createResponse.Name, response.Name);
@@ -78,9 +78,9 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
         public async Task DeleteAsync_Successfully_ReturnEntity(CarbonContextTestClass carbonContextTestClass)
         {
             // Arrange
-            var createResponse = await base.CreateAsync(carbonContextTestClass);
+            var createResponse = await base.CreateAsync(carbonContextTestClass, CancellationToken.None);
             // Act
-            var response = await base.DeleteAsync(createResponse.Id);
+            var response = await base.DeleteAsync(createResponse.Id, CancellationToken.None);
 
             // Assert
             Assert.Equal(carbonContextTestClass.Id, response.Id);
@@ -92,7 +92,7 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
             // Arrange
             EFRepositoryFixture.CreateData(base.context, Guid.NewGuid(), Guid.NewGuid());
             // Act
-            var response = await base.GetAllAsync();
+            var response = await base.GetAllAsync(CancellationToken.None);
 
             // Assert
             Assert.IsType<List<CarbonContextTestClass>>(response);
@@ -105,7 +105,7 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
             // Arrange
 
             // Act
-            var response = await base.CreateRangeAsync(carbonContextTestClassList);
+            var response = await base.CreateRangeAsync(carbonContextTestClassList, CancellationToken.None);
 
             // Assert
             Assert.IsType<List<CarbonContextTestClass>>(response);
@@ -116,14 +116,14 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
         public async Task UpdateRangeAsync_Successfully_ReturnEntity(List<CarbonContextTestClass> carbonContextTestClassList)
         {
             // Arrange
-            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList);
+            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList, CancellationToken.None);
             foreach (var item in createResponse)
             {
                 var i = 0;
                 item.Name += i++.ToString();
             }
             // Act
-            var response = await base.UpdateRangeAsync(createResponse);
+            var response = await base.UpdateRangeAsync(createResponse, CancellationToken.None);
 
             // Assert
             for (var a = 0; a < response.Count; a++)
@@ -138,10 +138,10 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
         public async Task DeleteRangeAsync_Successfully_ReturnEntity(List<CarbonContextTestClass> carbonContextTestClassList)
         {
             // Arrange
-            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList);
+            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList, CancellationToken.None);
 
             // Act
-            var response = await base.DeleteRangeAsync(createResponse);
+            var response = await base.DeleteRangeAsync(createResponse, CancellationToken.None);
 
             // Assert
             Assert.IsType<List<CarbonContextTestClass>>(response);
@@ -152,10 +152,10 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
         public async Task GetAsync_Successfully_ReturnEntity(List<CarbonContextTestClass> carbonContextTestClassList)
         {
             // Arrange
-            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList);
+            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList, CancellationToken.None);
 
             // Act
-            var response = await base.GetAsync(x => x.Name == "Test Name 1");
+            var response = await base.GetAsync(x => x.Name == "Test Name 1", CancellationToken.None);
 
             // Assert
             Assert.Equal("Test Name 1", response.Name);
@@ -166,7 +166,7 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
         public async Task Query_Successfully_ReturnEntity(List<CarbonContextTestClass> carbonContextTestClassList)
         {
             // Arrange
-            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList);
+            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList, CancellationToken.None);
 
             // Act
             var response = base.Query().ToList();
@@ -180,7 +180,7 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
         public async Task QueryAsNoTracking_Successfully_ReturnEntity(List<CarbonContextTestClass> carbonContextTestClassList)
         {
             // Arrange
-            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList);
+            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList, CancellationToken.None);
 
             // Act
             var response = base.QueryAsNoTracking().ToList();
@@ -194,10 +194,10 @@ namespace Carbon.Domain.EntityFrameworkCore.UnitTests
         public async Task SaveChangesAsync_Successfully_ReturnInteger(List<CarbonContextTestClass> carbonContextTestClassList)
         {
             // Arrange
-            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList);
+            var createResponse = await base.CreateRangeAsync(carbonContextTestClassList, CancellationToken.None);
             base.context.CarbonContextTestClass.Add(new CarbonContextTestClass { Id = Guid.NewGuid(), Name = "Name 3" });
             // Act
-            var response = await base.SaveChangesAsync();
+            var response = await base.SaveChangesAsync(CancellationToken.None);
 
             // Assert
             Assert.IsType<int>(response);
