7605efb3fb782e234ff77d07936e4afa0b8aa6ce
Ahmet Güner <ahmet.guner@kocdigital.com>
2025-11-06 15:06:34 +0300

    feat: Refactored OrdinatedPageDto to synchronize Orderables and Ordination properties

--- Carbon.Common/OrdinatedPageDto.cs
+++ Carbon.Common/OrdinatedPageDto.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace Carbon.Common
@@ -8,10 +7,46 @@ namespace Carbon.Common
     /// </summary>
     public class OrdinatedPageDto : IOrdinatedPageDto
     {
-         /// <summary>
-        /// List of Ordination data
+        private IList<Ordination> _ordination = new List<Ordination>();
+        private IList<Orderable> _orderables = new List<Orderable>();
+        /// <summary>
+        /// Orderables property. Assigning a non-empty list also updates Ordination.
         /// </summary>
-        public IList<Orderable> Orderables { get; set; } = new List<Orderable>();
+        public IList<Orderable> Orderables
+        {
+            get => _orderables;
+            set
+            {
+                _orderables = value ?? new List<Orderable>();
+                if (_orderables.Count > 0)
+                {
+                    _ordination = new List<Ordination>();
+                    foreach (var o in _orderables)
+                    {
+                        _ordination.Add(new Ordination { Value = o.Value, IsAscending = o.IsAscending });
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Ordination property. Assigning a non-empty list also updates Orderables.
+        /// </summary>
+        public IList<Ordination> Ordination
+        {
+            get => _ordination;
+            set
+            {
+                _ordination = value ?? new List<Ordination>();
+                if (_ordination.Count > 0)
+                {
+                    _orderables = new List<Orderable>();
+                    foreach (var o in _ordination)
+                    {
+                        _orderables.Add(new Orderable { Value = o.Value, IsAscending = o.IsAscending });
+                    }
+                }
+            }
+        }
         /// <summary>
         /// Size of data of each page
         /// </summary>
