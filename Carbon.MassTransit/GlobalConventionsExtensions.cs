1f83960fbc111bdd3ad9336560dd315bb22fafa6
Ahmet Alperen Atasever <ahmetalperen.atasever@kocdigital.com>
2026-04-17 11:10:46 +0300

    fix: audit event path

--- Carbon.MassTransit/GlobalConventionsExtensions.cs
+++ Carbon.MassTransit/GlobalConventionsExtensions.cs
@@ -1,12 +1,12 @@
 using MassTransit.RabbitMqTransport;
-
+using Carbon.Audit.Contracts;
 namespace Carbon.MassTransit;
 
 public static class GlobalConventionsExtensions
 {
     public static void ConfigureGlobalMessageMappings(this IRabbitMqBusFactoryConfigurator cfg)
     {
-        cfg.Message<Audit.Contracts.AuditEvent>(m => m.SetEntityName("audit.events"));
-        cfg.Publish<Audit.Contracts.AuditEvent>(p => p.ExchangeType = "fanout");
+        cfg.Message<AuditEvent>(m => m.SetEntityName("audit.events"));
+        cfg.Publish<AuditEvent>(p => p.ExchangeType = "fanout");
     }
 }
